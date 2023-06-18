20cd12d54b655929b4f3f240f39ddd90bb18a67e
malav-mehta <malavhmehta@outlook.com>
2023-06-17 23:12:57 -0400

    Add sample query for R9b

--- sql/features/4_view_snapshot/test.sql
+++ sql/features/4_view_snapshot/test.sql
@@ -1,1 +1,21 @@
-SELECT * FROM User;
+SELECT
+    date_trunc('hour', T.createdAt) AS atTime,
+    T.rowCount,
+    T.sizeBytes,
+    COUNT(Q1.atHour) AS queryCount
+FROM
+    TableStorageSnapshot T
+LEFT JOIN
+    (SELECT
+        Q1.tableId,
+        date_trunc('hour', Q1.issuedAt) AS atHour
+     FROM
+        Query Q1) Q
+ON
+    T.tableId = Q.tableId AND date_trunc('hour', T.createdAt) = Q.atHour
+WHERE
+    T.tableId = INtableId AND
+    INfromTime <= T.createdAt AND
+    T.createdAt <= INtoTime
+GROUP BY
+    T.tableId, T.createdAt;
