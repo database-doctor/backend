5b8fa452d2f77a0cf231aa5da6b11f4ff853d946
malav-mehta <malavhmehta@outlook.com>
2023-06-17 23:15:37 -0400

    Use quotations for query for R9b

--- sql/features/4_view_snapshot/test.sql
+++ sql/features/4_view_snapshot/test.sql
@@ -1,21 +1,21 @@
 SELECT
-    date_trunc('hour', T.createdAt) AS atTime,
-    T.rowCount,
-    T.sizeBytes,
-    COUNT(Q1.atHour) AS queryCount
+    date_trunc('hour', T."createdAt") AS "atTime",
+    T."rowCount",
+    T."sizeBytes",
+    COUNT(Q1."atHour") AS "queryCount"
 FROM
-    TableStorageSnapshot T
+    "TableStorageSnapshot" T
 LEFT JOIN
     (SELECT
-        Q1.tableId,
-        date_trunc('hour', Q1.issuedAt) AS atHour
+        Q1."tableId",
+        date_trunc('hour', Q1."issuedAt") AS "atHour"
      FROM
-        Query Q1) Q
+        "Query" Q1) Q
 ON
-    T.tableId = Q.tableId AND date_trunc('hour', T.createdAt) = Q.atHour
+    T."tableId" = Q."tableId" AND date_trunc('hour', T."createdAt") = Q."atHour"
 WHERE
-    T.tableId = INtableId AND
-    INfromTime <= T.createdAt AND
-    T.createdAt <= INtoTime
+    T."tableId" = 1 AND
+    '2023-01-01'::date <= T."createdAt" AND
+    T."createdAt" <= '2023-12-31'::date
 GROUP BY
-    T.tableId, T.createdAt;
+    T."tableId", T."createdAt";
