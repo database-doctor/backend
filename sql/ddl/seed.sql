a30e74925adde10b7891c7abddbb5b638e9d06d7
Adrian Davila <akdavila03@gmail.com>
2023-06-19 10:59:22 -0400

    add inserting users to seed data

--- sql/ddl/seed.sql
+++ sql/ddl/seed.sql
@@ -1,1 +1,5 @@
-INSERT INTO 
+-- Inserting sample users into the table.
+INSERT INTO "User" ("username", "name", "email", "passwordHash", "passwordSalt") VALUES ('adriandavila', 'Adrian Davila', 'a4davila@uwaterloo.ca', 'hashed_password', 'salt');
+INSERT INTO "User" ("username", "name", "email", "passwordHash", "passwordSalt") VALUES ('jasondu', 'Jason Du', 'j79du@uwaterloo.ca', 'hashed_password', 'salt');
+INSERT INTO "User" ("username", "name", "email", "passwordHash", "passwordSalt") VALUES ('malavmehta', 'Malav Mehta', 'mmehta@uwaterloo.ca', 'hashed_password', 'salt');
+
