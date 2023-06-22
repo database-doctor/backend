7639c4724d5f8144d14cafdfc36083e2f9c8a31a
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2023-06-21 22:59:52 -0400

    fix test.sql

--- sql/features/3_view_common_queries/test.sql
+++ sql/features/3_view_common_queries/test.sql
@@ -1,32 +1,32 @@
 -- View commonly queried statements
 WITH "QueryCount" AS 
-    (SELECT "statement", COUNT(*) AS "query_count" 
+    (SELECT "statement", COUNT(*) AS "queryCount" 
      FROM "Query" 
      GROUP BY "statement"), 
-"avg_query_count" AS 
-    (SELECT AVG("query_count") FROM "QueryCount"), 
+"avgQueryCount" AS 
+    (SELECT AVG("queryCount") FROM "QueryCount"), 
 "CommonQueries" AS 
     (SELECT "statement"
      FROM "QueryCount"
-     WHERE "query_count" >= "avg_query_count")
-SELECT DISTINCT "User"."username", "Project"."project_name", "Table"."table_accessed", "Q"."statement" AS "query_statement", "QueryType"."query_type_name" AS "query_type", "Q"."issued_at", "Q"."finished_at", "Q"."has_error", "Q".'error_msg'
+     WHERE "queryCount" >= "avgQueryCount")
+SELECT DISTINCT "User"."username", "Project"."projectName", "Q"."statement" AS "queryStatement", "QueryType"."queryTypeName" AS "queryType", "Q"."issuedAt", "Q"."finishedAt", "Q"."hasError", "Q".'errorMsg'
 FROM "Query" "Q"
-JOIN "QueryType" ON "Query"."query_type" = "QueryType"."query_type_id"
+JOIN "QueryType" ON "Query"."queryType" = "QueryType"."queryTypeId"
 JOIN "User" ON "Query"."userId" = "User"."userId"
 JOIN "Project" ON "Query"."projectId" = "Project"."projectId"
 WHERE "Query"."statement" IN "CommonQueries";
 
 -- View common users that query
 WITH "UserQueries" AS 
-    (SELECT "user_id", COUNT(*) AS "query_count" 
+    (SELECT "userId", COUNT(*) AS "queryCount" 
      FROM "Query" 
-     GROUP BY "user_id"), 
-"avg_query_count" AS 
-    (SELECT AVG("query_count") FROM "UserQueries"), 
+     GROUP BY "userId"), 
+"avgQueryCount" AS 
+    (SELECT AVG("queryCount") FROM "UserQueries"), 
 "CommonUsers" AS 
     (SELECT "user_id"
      FROM "UserQueries"
-     WHERE "query_count" >= "avg_query_count")
+     WHERE "queryCount" >= "avgQueryCount")
 SELECT DISTINCT "User"."username"
 FROM "User"
 WHERE "User"."user_id" IN "CommonUsers";
