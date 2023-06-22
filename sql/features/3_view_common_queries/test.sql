7d621e66da0bf4f9aebfce0743f312eb34072e35
Adrian Davila <akdavila03@gmail.com>
2023-06-21 23:10:47 -0400

    debugging query

--- sql/features/3_view_common_queries/test.sql
+++ sql/features/3_view_common_queries/test.sql
@@ -2,31 +2,25 @@
 WITH "QueryCount" AS 
     (SELECT "statement", COUNT(*) AS "queryCount" 
      FROM "Query" 
-     GROUP BY "statement"), 
-"avgQueryCount" AS 
-    (SELECT AVG("queryCount") FROM "QueryCount"), 
-"CommonQueries" AS 
-    (SELECT "statement"
-     FROM "QueryCount"
-     WHERE "queryCount" >= "avgQueryCount")
-SELECT DISTINCT "User"."username", "Project"."projectName", "Q"."statement" AS "queryStatement", "QueryType"."queryTypeName" AS "queryType", "Q"."issuedAt", "Q"."finishedAt", "Q"."hasError", "Q".'errorMsg'
+     GROUP BY "statement")
+SELECT DISTINCT "User"."username", "Project"."projectName", "Q"."statement" AS "queryStatement", "QueryType"."queryTypeName" AS "queryType", "Q"."issuedAt", "Q"."finishedAt", "Q"."hasError", "Q"."errorMessage"
 FROM "Query" "Q"
-JOIN "QueryType" ON "Query"."queryType" = "QueryType"."queryTypeId"
-JOIN "User" ON "Query"."userId" = "User"."userId"
-JOIN "Project" ON "Query"."projectId" = "Project"."projectId"
-WHERE "Query"."statement" IN "CommonQueries";
+JOIN "QueryType" ON "Q"."queryTypeId" = "QueryType"."queryTypeId"
+JOIN "User" ON "Q"."userId" = "User"."userId"
+JOIN "Project" ON "Q"."projectId" = "Project"."projectId"
+WHERE "Q"."statement" IN (SELECT "statement" FROM "QueryCount" WHERE "queryCount" >= (SELECT AVG("queryCount") FROM "QueryCount"));
 
 -- View common users that query
-WITH "UserQueries" AS 
-    (SELECT "userId", COUNT(*) AS "queryCount" 
-     FROM "Query" 
-     GROUP BY "userId"), 
-"avgQueryCount" AS 
-    (SELECT AVG("queryCount") FROM "UserQueries"), 
-"CommonUsers" AS 
-    (SELECT "user_id"
-     FROM "UserQueries"
-     WHERE "queryCount" >= "avgQueryCount")
-SELECT DISTINCT "User"."username"
-FROM "User"
-WHERE "User"."user_id" IN "CommonUsers";
+-- WITH "UserQueries" AS 
+--     (SELECT "userId", COUNT(*) AS "queryCount" 
+--      FROM "Query" 
+--      GROUP BY "userId"), 
+-- "avgQueryCount" AS 
+--     (SELECT AVG("queryCount") FROM "UserQueries"), 
+-- "CommonUsers" AS 
+--     (SELECT "userId"
+--      FROM "UserQueries"
+--      WHERE "queryCount" >= "avgQueryCount")
+-- SELECT DISTINCT "User"."username"
+-- FROM "User"
+-- WHERE "User"."userId" IN "CommonUsers"."userId";
