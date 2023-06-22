8424f2eeb356212c29228ed4a2a6224f8a4ef53b
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2023-06-21 22:36:08 -0400

    Insert seed for project

--- sql/ddl/seed.sql
+++ sql/ddl/seed.sql
@@ -4,5 +4,5 @@ INSERT INTO "User" ("username", "name", "email", "passwordHash", "passwordSalt")
     ('jasondu', 'Jason Du', 'j79du@uwaterloo.ca', 'hashed_password', 'salt'),
     ('malavmehta', 'Malav Mehta', 'mmehta@uwaterloo.ca', 'hashed_password', 'salt');
 
-
-
+INSERT INTO "Project" ("projectName", "connUrl", "createdById") VALUES
+    ('TestProject', 'http://example-url.com', '1');
