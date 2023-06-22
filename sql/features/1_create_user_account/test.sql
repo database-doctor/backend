db072d42dcf2d8a822fed9ec0c7ce686f6833d26
Adrian Davila <akdavila03@gmail.com>
2023-06-21 22:21:22 -0400

    R6

--- sql/features/1_create_user_account/test.sql
+++ sql/features/1_create_user_account/test.sql
@@ -1,1 +1,12 @@
-SELECT * FROM User;
+-- Creating a user account
+INSERT INTO "User"("username", "name", "email", "passwordHash", "passwordSalt") VALUES ('new_user', 'New User', 'new_user@uwaterloo.ca', 'hashed_password', 'salt');
+
+-- Modifying a user account
+UPDATE "User"
+SET "email" = 'akdavila03@gmail.com'
+WHERE "userId" = 1;
+
+-- Accessing profile information sample query
+SELECT "username", "name", "email", "createdAt"
+FROM "User"
+WHERE "userId" = 1;
