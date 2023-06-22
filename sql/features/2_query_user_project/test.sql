ef9c8e62bc840b706826abc8689343d6f4a7c144
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2023-06-21 22:48:17 -0400

    createdById -> userId

--- sql/features/2_query_user_project/test.sql
+++ sql/features/2_query_user_project/test.sql
@@ -1,4 +1,4 @@
 SELECT "projectName", "connUrl" AS "projectUrl", "createdAt", "username" AS "createdBy"
 FROM "Project"
-JOIN "User" ON "Project"."createdById" = "User"."createdById"
+JOIN "User" ON "Project"."createdById" = "User"."userId"
 WHERE "projectName" = 'project_query_string'
