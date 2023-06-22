147c1bfdcece27f317680e50c0a7f6f567e49273
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2023-06-21 22:26:52 -0400

    initial test queries

--- sql/features/3_view_common_queries/test.sql
+++ sql/features/3_view_common_queries/test.sql
@@ -1,16 +1,70 @@
+-- View commonly queried statements
 WITH QueryCount AS 
-    (SELECT query_type, COUNT(*) AS query_count 
+    (SELECT "statement", COUNT(*) AS query_count 
      FROM Query 
-     GROUP BY query_type)
+     GROUP BY "statement")
 WITH avg_query_count AS 
     (SELECT AVG(query_count) FROM QueryCount)
 WITH CommonQueries AS 
-    (SELECT query_type_id
+    (SELECT "statement"
      FROM QueryCount
      WHERE query_count >= avg_query_count)
-SELECT username AS "user", project_name, statement AS "query_statement", query_type_name AS "query_type", issued_at, finished_at, has_error, error_msg
-FROM Query
-NATURAL JOIN QueryType
+SELECT DISTINCT User.username, Project.project_name, Table.table_accessed, Q.statement AS "query_statement", QueryType.query_type_name AS "query_type", Q.issued_at, Q.finished_at, Q.has_error, Q.error_msg
+FROM Query Q
+JOIN QueryType ON Query.query_type = QueryType.query_type_id
 NATURAL JOIN "User"
 NATURAL JOIN Project
-WHERE query_type_id IN CommonQueries.query_type_id
+WHERE Query."statement" IN CommonQueries;
+
+-- View common users that query
+WITH UserQueries AS 
+    (SELECT user_id, COUNT(*) AS query_count 
+     FROM Query 
+     GROUP BY user_id)
+WITH avg_query_count AS 
+    (SELECT AVG(query_count) FROM UserQueries)
+WITH CommonUsers AS 
+    (SELECT user_id
+     FROM UserQueries
+     WHERE query_count >= avg_query_count)
+SELECT DISTINCT User.username
+FROM User
+WHERE User.user_id IN CommonUsers;
+
+-- View commonly queried columns
+WITH QueryCount AS 
+    (SELECT "statement", COUNT(*) AS query_count 
+     FROM Query 
+     GROUP BY "statement")
+WITH avg_query_count AS 
+    (SELECT AVG(query_count) FROM QueryCount)
+WITH CommonQueries AS 
+    (SELECT "statement"
+     FROM QueryCount
+     WHERE query_count >= avg_query_count)
+SELECT DISTINCT User.username, Project.project_name, Table.table_accessed, Q.statement AS "query_statement", QueryType.query_type_name AS "query_type", Q.issued_at, Q.finished_at, Q.has_error, Q.error_msg
+FROM Query Q
+JOIN QueryType ON Query.query_type = QueryType.query_type_id
+NATURAL JOIN "User"
+NATURAL JOIN Project
+WHERE Query."statement" IN CommonQueries;
+
+-- View commonly queried tables
+WITH QueryTable AS 
+    (SELECT table_id, COUNT(*) AS query_count 
+     FROM QueryTableAccess 
+     GROUP BY table_id)
+WITH avg_query_count AS 
+    (SELECT AVG(query_count) FROM QueryTable)
+WITH CommonTableQueries AS 
+    (SELECT table_id
+     FROM QueryTable
+     WHERE query_count >= avg_query_count)
+SELECT DISTINCT User.username, Project.project_name, Table.table_accessed, Q.statement AS "query_statement", QueryType.query_type_name AS "query_type", Q.issued_at, Q.finished_at, Q.has_error, Q.error_msg
+FROM Query Q
+JOIN QueryType ON Query.query_type = QueryType.query_type_id
+NATURAL JOIN "User"
+NATURAL JOIN Project
+NATURAL JOIN QueryTableAccess
+NATURAL JOIN "Table"
+WHERE Query."statement" IN CommonQueries;
