ed552ba5561da5bd654da7cac3cac44bdf23b551
Adrian Davila <akdavila03@gmail.com>
2023-06-22 21:51:24 -0400

    fix query

--- sql/features/2_query_user_project/test.sql
+++ sql/features/2_query_user_project/test.sql
@@ -1,4 +1,4 @@
 SELECT "projectName", "connUrl" AS "projectUrl", "Project"."createdAt", "username" AS "createdBy"
 FROM "Project"
 JOIN "User" ON "Project"."createdById" = "User"."userId"
-WHERE "projectName" = 'project_query_string';
+WHERE "projectId" = 'project_id_value';
