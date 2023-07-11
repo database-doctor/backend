7a6f0d9625d6aa992414686cee67fe0fdb9c2e34
AarushKhurana <a23khurana@uwaterloo.ca>
2023-07-10 22:54:23 -0400

    ADDED all select statements

--- sql/ddl/sample.sql
+++ sql/ddl/sample.sql
@@ -5,22 +5,16 @@ INSERT INTO "User" ("userId", "username", "name", "email", "passwordHash", "pass
     ('18', 'malavmehta', 'Malav Mehta', 'mmehta@uwaterloo.ca', 'hashed_password', 'salt');
 
 INSERT INTO "Project" ("projectId", "projectName", "connUrl", "createdById") VALUES
-    ('6', 'TestProject', 'http://example-url.com', '1');
-
-INSERT INTO "QueryType" ("queryTypeId", "queryTypeName") VALUES
-    ('1', 'SELECT'),
-    ('2', 'INSERT'),
-    ('3', 'UPDATE'),
-    ('4', 'DELETE');
+    ('6', 'TestProject', 'http://example-url.com', '16');
 
 INSERT INTO "SqlQuery" ("queryId", "statement", "userId", "projectId", "queryTypeId") VALUES
-    ('1', 'SELECT * FROM Table', '1', '1', '1');
+    ('1', 'SELECT * FROM Table', '1', '6', '1');
 
 INSERT INTO "Schema" ("schemaId", "schemaName", "projectId", "createdById") VALUES
-    ('1', 'TestSchema', '1', '1');
+    ('6', 'TestSchema', '6', '1');
 
 INSERT INTO "Table" ("tableId", "tableName", "schemaId") VALUES 
-    ('1', 'TestTable', '1');
+    ('1', 'TestTable', '6');
 
 INSERT INTO "QueryTableAccess" ("queryId", "tableId") VALUES 
     ('1', '1');
@@ -31,12 +25,5 @@ INSERT INTO "TableStorageSnapshot" ("rowCount", "sizeBytes", "tableId") VALUES
 INSERT INTO "UserProjectToken" ("userId", "projectId", "accessToken") VALUES
     ('2', '1', 'SecretToken');
 
-INSERT INTO "ColumnType" ("columnTypeId", "columnTypeName") VALUES
-    ('1', 'INTEGER'),
-    ('2', 'VARCHAR'),
-    ('3', 'FLOAT'),
-    ('4', 'BOOLEAN'),
-    ('5', 'DATE');
-
 INSERT INTO "Column" ("columnId", "columnName", "columnTypeId", "tableId") VALUES
-    ('1', 'TestColumn', '1', '1');
+    ('1', 'TestColumn', '7', '1');
