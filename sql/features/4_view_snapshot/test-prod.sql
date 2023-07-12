9bbfe7060c7fd4958612b06f3447433a9ab3f1b5
malav-mehta <malavhmehta@outlook.com>
2023-07-11 20:42:08 -0400

    Create prod query for R9

---
+++ sql/features/4_view_snapshot/test-prod.sql
@@ -0,0 +1,25 @@
+SELECT
+    date_trunc('hour', T."createdAt") AS "atTime",
+    T."rowCount",
+    T."sizeBytes",
+    COUNT(Q."atHour") AS "queryCount"
+FROM
+    "TableStorageSnapshot" T
+LEFT JOIN
+    "QueryTableAccess" TA
+ON
+    T."tableId" = TA."tableId"
+LEFT JOIN
+    (SELECT
+        Q1."queryId",
+        date_trunc('hour', Q1."issuedAt") AS "atHour"
+     FROM
+        "SqlQuery" Q1) Q
+ON
+    TA."queryId" = Q."queryId" AND date_trunc('hour', T."createdAt") = Q."atHour"
+WHERE
+    T."tableId" = 12 AND
+    '2023-01-01'::date <= T."createdAt" AND
+    T."createdAt" <= '2023-12-31'::date
+GROUP BY
+    T."tableId", T."createdAt";
