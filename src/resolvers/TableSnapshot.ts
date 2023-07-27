88ca0b3cef78463ea1196e55091835b86d704d77
malav-mehta <malavhmehta@outlook.com>
2023-07-27 00:11:55 -0400

    Update Table Snapshot resolver

--- src/resolvers/TableSnapshot.ts
+++ src/resolvers/TableSnapshot.ts
@@ -10,6 +10,18 @@ import {
   ObjectType,
 } from "type-graphql";
 
+@InputType()
+class TableSnapshotInput {
+  @Field(() => Int)
+  tid!: number;
+
+  @Field(() => String)
+  fromTime!: string;
+
+  @Field(() => String)
+  toTime!: string;
+}
+
 @ObjectType()
 class TableSnapshotOutput {
   @Field(() => Date)
@@ -25,53 +37,50 @@ class TableSnapshotOutput {
   queryCount!: number;
 }
 
-@InputType()
-class TableSnapshotInput {
-  @Field(() => Int)
-  tid!: number;
-
-  @Field(() => String)
-  fromTime!: string;
-
-  @Field(() => String)
-  toTime!: string;
-}
-
 @Resolver(() => TableSnapshotOutput)
 export class TableSnapshotResolver {
   @Query(() => [TableSnapshotOutput])
   async tableSnapshots(
     @Arg("table") table: TableSnapshotInput,
-    @Ctx() ctx: Context,
+    @Ctx() ctx: Context
   ): Promise<TableSnapshotOutput[]> {
     const rawSnapshots = await ctx.prisma.$queryRawUnsafe(`
       SELECT
-          date_trunc('hour', TS."createdAt") AS "atTime",
-          TS."rowCount",
-          TS."sizeBytes",
-          COUNT(J."atHour") AS "queryCount"
+          date_trunc('hour', "TS"."createdAt") AS "atTime",
+          MAX("TS"."rowCount") AS "rowCount",
+          MAX("TS"."sizeBytes") AS "sizeBytes",
+          COUNT("J"."atHour") AS "queryCount"
       FROM
-          "TableSnapshot" TS
+          "TableSnapshot" "TS"
       LEFT JOIN
-          "JobTableAccess" JTA
+          "JobTableAccess" "JTA"
       ON
-          TS."tid" = JTA."tid"
+          "TS"."tid" = "JTA"."tid"
       LEFT JOIN
           (SELECT
-              J1."jid",
-              date_trunc('hour', J1."issuedAt") AS "atHour"
+              "J1"."jid",
+              date_trunc('hour', "J1"."issuedAt") AS "atHour"
           FROM
-              "Job" J1) J
+              "Job" "J1") "J"
       ON
-          JTA."jid" = J."jid" AND date_trunc('hour', TS."createdAt") = J."atHour"
+          "JTA"."jid" = "J"."jid" AND date_trunc('hour', "TS"."createdAt") = "J"."atHour"
       WHERE
-          TS."tid" = ${table.tid} AND
-          '${table.fromTime}'::date <= TS."createdAt" AND
-          TS."createdAt" <= '${table.toTime}'::date
+          "TS"."tid" = '${table.tid}' AND
+          '${table.fromTime}'::date <= "TS"."createdAt" AND
+          "TS"."createdAt" <= '${table.toTime}'::date
       GROUP BY
-          TS."tid", date_trunc('hour', TS."createdAt") AS "atTime";
+          date_trunc('hour', "TS"."createdAt")
+      ORDER BY
+        date_trunc('hour', "TS"."createdAt") ASC;
     `);
 
-    return rawSnapshots as TableSnapshotOutput[];
+    const snapshots = (rawSnapshots as any[]).map((rawSnapshot) => ({
+      atTime: rawSnapshot.atTime,
+      rowCount: Number(rawSnapshot.rowCount),
+      sizeBytes: Number(rawSnapshot.sizeBytes),
+      queryCount: Number(rawSnapshot.queryCount),
+    }));
+
+    return snapshots as TableSnapshotOutput[];
   }
 }
